49053b82c5f4a546de87ab7ee1a06dc567e03bc8
tails618 <asher.grossman@gmail.com>
2022-01-22 14:09:30 -0600

    Imported to 2022. Work in NEW folder.

---
+++ Cache-Money-Rapid-React-2022-NEW/src/main/java/frc/robot/subsystems/Arm.java
@@ -0,0 +1,21 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj.Servo;
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+public class Arm extends Subsystem{
+    public static Servo armMotor = new Servo(0);
+
+    @Override
+    protected void initDefaultCommand() {
+    }
+    public void goToDefault(){
+        armMotor.setAngle(0.0);
+    }
+    public void setAngle(double degrees){
+        armMotor.setAngle(degrees);
+    }
+    public double getAngle(){
+        return armMotor.getAngle();
+    }
+}
